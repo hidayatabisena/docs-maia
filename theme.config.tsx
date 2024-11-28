91f08d3873565c0012881d52523dd2c0f3898c28
Han Sena <hansena@moyahexagon.com>
2024-11-28 23:59:10 +0700

    Working progress to write more useful tutorial to our clients

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,23 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>MAIA Docs</span>,
+  head: ( 
+    <> 
+      <meta property="og:title" content="MAIA Documentation" />
+      <meta property="og:description" content="Mamperkenalkan MAIA, sebuah AI Platform yang memungkinkan tim atau perusahaan untuk membuat ChatGPT versi mereka sendiri." />
+      <meta property="og:image" content="https://res.cloudinary.com/moyadev/image/upload/v1732762168/maia/FEED_C_wrg3kn.jpg" />
+    </> 
+  ), 
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/MAIA-ID',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'MAIA Documentations © 2024',
   },
 }
 
