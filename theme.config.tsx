8530ff8a1256dca9c5bad23b69e551ccbad9c0e4
Han Sena <hansena@moyahexagon.com>
2024-12-02 09:46:46 +0700

    Removing Nextra suffix on metatag and meta title

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s - MAIA.ID'
+    }
+  },
   logo: <span>MAIA Docs</span>,
   head: ( 
     <> 
